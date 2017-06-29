1a587efb7a8507fbe79eb422abb439f26aa41d0b
Danny Malter <danmalter@users.noreply.github.com>
2017-06-29 15:56:01 -0500

    Update hideOutput.js

--- javascripts/hideOutput.js
+++ javascripts/hideOutput.js
@@ -1,17 +1,39 @@
-$(document).ready(function() {
-  $output = $("pre").not(".r");
-  $output.prepend("<div class='.showopt'>Show Output</div><br/>");
-  $output.children("code").css({display: "none"});
-            
-  $(".showopt").click(function() {
-    $btn = $(this);
-    $chunk = $(this).parent().children("code");
-    if($btn.html() === "Show Output") {
-      $btn.html("Hide Output");
-    } else {
-      $btn.html("Show Output");
+(document).ready(function() {
+
+  $chunks = $('.fold');
+
+  $chunks.each(function () {
+
+    // add button to source code chunks
+    if ( $(this).hasClass('s') ) {
+      $('pre.r', this).prepend("<div class=\"showopt\">Show Source</div><br style=\"line-height:22px;\"/>");
+      $('pre.r', this).children('code').attr('class', 'folded');
     }
-    $chunk.slideToggle("fast", "swing");
+
+    // add button to output chunks
+    if ( $(this).hasClass('o') ) {
+      $('pre:not(.r)', this).has('code').prepend("<div class=\"showopt\">Show Output</div><br style=\"line-height:22px;\"/>");
+      $('pre:not(.r)', this).children('code:not(r)').addClass('folded');
+
+      // add button to plots
+      $(this).find('img').wrap('<pre class=\"plot\"></pre>');
+      $('pre.plot', this).prepend("<div class=\"showopt\">Show Plot</div><br style=\"line-height:22px;\"/>");
+      $('pre.plot', this).children('img').addClass('folded');
+
+    }
+  });
+
+  // hide all chunks when document is loaded
+  $('.folded').css('display', 'none')
+
+  // function to toggle the visibility
+  $('.showopt').click(function() {
+    var label = $(this).html();
+    if (label.indexOf("Show") >= 0) {
+      $(this).html(label.replace("Show", "Hide"));
+    } else {
+      $(this).html(label.replace("Hide", "Show"));
+    }
+    $(this).siblings('code, img').slideToggle('fast', 'swing');
   });
 });
-  
